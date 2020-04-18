624cd9a8b542da404014e113c18aff8723bf58be
Yaroslav Spirin <yar.spirin@gmail.com>
2020-04-18 22:32:10 +0300

    Add Travis CI config file

---
+++ CoreImageExperiments/Calculator.swift
@@ -0,0 +1,9 @@
+//
+//  Calculator.swift
+//  CoreImageExperiments
+//
+//  Created by Yaroslav Spirin on 18/04/2020.
+//  Copyright © 2020 Yaroslav Spirin. All rights reserved.
+//
+
+import Foundation
